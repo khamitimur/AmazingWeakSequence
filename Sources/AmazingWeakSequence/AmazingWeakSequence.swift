e931573ceab9061e35dbcb4eda0dd9cd4978e2bb
Timur Khamidov <baton237@gmail.com>
2021-03-30 07:44:24 +0300

    Add convenience init with initial elements

--- Sources/AmazingWeakSequence/AmazingWeakSequence.swift
+++ Sources/AmazingWeakSequence/AmazingWeakSequence.swift
@@ -25,6 +25,13 @@ public final class AmazingWeakSequence<Element>: Sequence {
         self.semaphore = DispatchSemaphore(value: 1)
     }
     
+    /// Initializes a new instance and adds initial elements to it.
+    public convenience init(elements: [Element]) {
+        self.init()
+        
+        self.add(elements)
+    }
+    
     // MARK: - Sequence
     
     public func makeIterator() -> AmazingWeakSequenceIterator<Element> {
@@ -42,10 +49,28 @@ public final class AmazingWeakSequence<Element>: Sequence {
         }
     }
     
+    /// Adds elements to the sequence.
+    public func add(_ elements: [Element]) {
+        semaphore.wait {
+            for element in elements {
+                weakHashTable.add(element as AnyObject)
+            }
+        }
+    }
+    
     /// Removes element from the sequence.
     public func remove(_ element: Element) {
         semaphore.wait {
             weakHashTable.remove(element as AnyObject)
+        }
+    }
+    
+    /// Removes elements from the sequence.
+    public func remove(_ elements: [Element]) {
+        semaphore.wait {
+            for element in elements {
+                weakHashTable.remove(element as AnyObject)
+            }
         }
     }
     
