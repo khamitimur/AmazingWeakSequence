b33a82026b81790e346c612226b25a95584ffe64
Timur Khamidov <baton237@gmail.com>
2021-03-30 09:12:32 +0300

    Add sample project

---
+++ AmazingWeakSequence/AmazingWeakSequence.h
@@ -0,0 +1,18 @@
+//
+//  AmazingWeakSequence.h
+//  AmazingWeakSequence
+//
+//  Created by t.khamidov on 30.03.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AmazingWeakSequence.
+FOUNDATION_EXPORT double AmazingWeakSequenceVersionNumber;
+
+//! Project version string for AmazingWeakSequence.
+FOUNDATION_EXPORT const unsigned char AmazingWeakSequenceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AmazingWeakSequence/PublicHeader.h>
+
+
